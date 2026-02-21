7faa623a65a0f1db9ae5c2e589b66c037d2e63b6
Andrew Drake <andrew@below43.com>
2026-02-21 16:04:30 +1300

    Updated SHA256 Hash

--- Formula/git-uncommit.rb
+++ Formula/git-uncommit.rb
@@ -2,7 +2,7 @@ class GitUncommit < Formula
   desc "Undo last git commit(s) but keep changes staged"
   homepage "https://github.com/below43/git-uncommit"
   url "https://github.com/below43/git-uncommit/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "2c439889de0bba4ce100b72b9ff989de1dbcf49d2741fe6f413282b2c340cd51"
+  sha256 "f4c5ddef677923b53092d525a18f24d1e9b4eab301163ae21dec2d784af1ca98"
   license "MIT"
 
   def install
